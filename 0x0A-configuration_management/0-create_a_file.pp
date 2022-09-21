b10e8da959b8b948cc93a4348d0c66dfadbd132c
Akeemandela <easytwenty14@gmail.com>
2022-09-21 05:21:38 -0700

    Configuration Management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,6 @@
+0-create_a_file{$tmp/school:
+                 ensure => "directory",
+                 owner => "www-data",
+                 group => "www-data",
+                 mode => 0744
+                }
