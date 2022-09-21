bfbbd8ada1d89b518d54e8c2cdcf64c5dec7bbcf
Akeemandela <easytwenty14@gmail.com>
2022-09-21 19:29:09 +0100

    Configuration Management Task

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,9 @@
-0-create_a_file{$tmp/school:
-                 ensure => "directory",
-                 owner => "www-data",
-                 group => "www-data",
-                 mode => 0744
-                }
+#Creating a file in Puppet
+file{ '/tmp/school':
+        ensure => file,
+	path => '/tmp/school',
+        owner => 'www-data',
+        group => 'www-data',
+        mode => '0744',
+        content => 'I love Puppet'        
+}
